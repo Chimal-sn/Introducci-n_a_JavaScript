567446cd4a41d25a586d223ca24d01af75a4c63d
Sony <sonychimo10@hotmail.com>
2026-04-28 17:11:10 -0600

    Update. Subir html

---
+++ tipos-de-dato.js
@@ -0,0 +1,23 @@
+console.log(typeof 42);
+console.log(typeof 'Veinticinco');
+console.log(typeof -666);
+console.log(typeof true);
+console.log(typeof 0);
+console.log(typeof '');
+console.log(typeof null);
+console.log(typeof undefined);
+console.log(typeof FALSE);
+
+const suma = 4 + 2;
+
+console.log(typeof suma);
+
+const sumaDos = "4" + 2;
+console.log(typeof sumaDos);
+
+const nombre = "Sony";
+console.log(typeof nombre);
+
+const saludo = `Hola ${nombre}`;
+console.log(typeof saludo);
+
